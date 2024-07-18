9b857f6d2d497900e7f184565b12cb568ac67b08
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-07-18 13:48:24 +0300

    Made conditional rendering about the games

--- client/src/components/game-list/GameList.jsx
+++ client/src/components/game-list/GameList.jsx
@@ -14,11 +14,12 @@ export default function GameList() {
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-            {/* Display div: with information about every game (if any) */}
-            {games.map(game => <GameListItem key={game._id} {...game} />)}
-            
-            {/* Display paragraph: If there is no games  */}
-            <h3 className="no-articles">No articles yet</h3>
+            {games.length > 0
+                ? games.map(game => <GameListItem key={game._id} {...game} />)
+                : <h3 className="no-articles">No games yet</h3>
+            }
+
+
         </section>
 
     );
