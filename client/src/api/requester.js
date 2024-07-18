abe1a5f2e4f5165def3077cb9263c815768309f7
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-07-18 13:23:33 +0300

    Add server

--- client/src/api/requester.js
+++ client/src/api/requester.js
@@ -24,3 +24,10 @@ export const get = requester.bind(null, 'GET');
 export const post = requester.bind(null, 'POST');
 export const put = requester.bind(null, 'PUT'); 
 export const del = requester.bind(null, 'DELETE');
+
+
+/*export const get = (url, data) => requester('GET', url, data);
+export const post = (url, data) => requester('POST', url, data);
+export const put = (url, data) => requester('PUT', url, data);
+export const del = (url, data) => requester('DELETE', url, data);
+*/
