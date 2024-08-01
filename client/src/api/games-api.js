11e5cbe228739c31af9191ce2b16454893b68324
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-08-01 17:23:53 +0300

    Added getLatest, remove and update in the games api

--- client/src/api/games-api.js
+++ client/src/api/games-api.js
@@ -10,6 +10,22 @@ export const getAll = async () => {
     return games;
 }
 
+export const getLatest = async () => {
+    const urlSearchParams = new URLSearchParams({
+        sortBy: '_createdOn desc',
+        pageSize: 3
+    });
+
+    const result = await request.get(`${BASE_URL}?${urlSearchParams.toString()}`);
+
+    const latestGames = Object.values(result);
+
+    return latestGames;
+}
 export const getOne = (gameId) => request.get(`${BASE_URL}/${gameId}`);
 
 export const create = (gameData) => request.post(`${BASE_URL}`, gameData);
+
+export const remove = (gameId) => request.del(`${BASE_URL}/${gameId}`);
+
+export const update = (gameId, gameData) => request.put(`${BASE_URL}/${gameId}`, gameData);
