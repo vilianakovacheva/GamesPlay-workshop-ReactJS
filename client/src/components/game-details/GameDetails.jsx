dd96922e62c2406f97748497d83601c7c01af2b7
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-08-07 12:32:06 +0300

    Edited the game details

--- client/src/components/game-details/GameDetails.jsx
+++ client/src/components/game-details/GameDetails.jsx
@@ -39,7 +39,7 @@ export default function GameDetails() {
         try {
             await gamesAPI.remove(gameId);
 
-            navigate('/games');
+            navigate('/');
         } catch (err) {
             console.log(err.message);
         }
